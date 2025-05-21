1bee5f396e830e54e6ba54cc72a9436b8340a6aa
rithikpradhan <pradhanrithik62@gmail.com>
2025-05-21 12:09:57 +0530

    github data commit

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
+const githubData = {
+  login: "mojombo",
+  id: 1,
+  node_id: "MDQ6VXNlcjE=",
+  avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/mojombo",
+  html_url: "https://github.com/mojombo",
+  followers_url: "https://api.github.com/users/mojombo/followers",
+  following_url: "https://api.github.com/users/mojombo/following{/other_user}",
+  gists_url: "https://api.github.com/users/mojombo/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/mojombo/subscriptions",
+  organizations_url: "https://api.github.com/users/mojombo/orgs",
+  repos_url: "https://api.github.com/users/mojombo/repos",
+  events_url: "https://api.github.com/users/mojombo/events{/privacy}",
+  received_events_url: "https://api.github.com/users/mojombo/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -17,6 +38,9 @@ app.get("/login", (req, res) => {
 
 app.get("/youtube", (req, res) => {
   res.send("<h2>Youtube</h2>");
+});
+app.get("/github", (req, res) => {
+  res.json(githubData);
 });
 
 app.listen(process.env.PORT, () => {
